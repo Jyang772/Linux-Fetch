babe61a1e8bf8864045a2b9cbfb7315b0153886b
Justin <jyang772@gmail.com>
2014-04-26 21:12:49 -0700

    New header

--- string.cpp
+++ string.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <string>
-#include "header.h"
+#include "Search.h"
 #include <stdlib.h>
+#include <fstream>
 
 //#DEFINE DEBUG
 using namespace std;
@@ -14,6 +15,28 @@ int main()
 	
 	Commands Input;
 	Input.Input();
+	
+	string line;
+	string buffer;
+	ifstream myfile;
+	myfile.open("test.txt");
+
+	while(!myfile.eof())
+	{	
+		
+		getline(myfile,line);
+		buffer += (line + "..");
+		cout << line << endl;
+	}
+	myfile.close();
+	cout << buffer << endl;
+
+	ofstream output;
+	output.open("test1.txt");
+	size_t found = buffer.find("..");
+	
+	output << buffer;
+	output.close();
 	
 	return 0;
 }
