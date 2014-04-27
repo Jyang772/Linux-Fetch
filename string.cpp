dc252d93654c2a8c7c4d9211f7b9499e4d54ead9
Justin <jyang772@gmail.com>
2014-04-27 00:28:09 -0700

    Parsing

--- string.cpp
+++ string.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include "Search.h"
+#include "Parser.h"
 #include <stdlib.h>
 #include <fstream>
 
@@ -16,27 +17,6 @@ int main()
 	Commands Input;
 	Input.Input();
 	
-	string line;
-	string buffer;
-	ifstream myfile;
-	myfile.open("test.txt");
-
-	while(!myfile.eof())
-	{	
-		
-		getline(myfile,line);
-		buffer += (line + "..");
-		cout << line << endl;
-	}
-	myfile.close();
-	cout << buffer << endl;
-
-	ofstream output;
-	output.open("test1.txt");
-	size_t found = buffer.find("..");
-	
-	output << buffer;
-	output.close();
-	
+	ParseText();
 	return 0;
 }
